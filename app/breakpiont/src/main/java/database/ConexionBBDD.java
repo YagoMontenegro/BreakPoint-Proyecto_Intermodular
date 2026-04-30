114b82033d0d91595310b37b5be6dc8cad0085d7
Yago Montenegro <y.montenegrodf@gmail.com>
2026-04-30 19:56:39 +0200

    Se añade ConexionBBDD.java

--- app/breakpiont/src/main/java/database/ConexionBBDD.java
+++ app/breakpiont/src/main/java/database/ConexionBBDD.java
@@ -1,4 +1,33 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBBDD {
+    private static Connection conexion;
+
+    public static Connection getConexion(){
+        try {
+            if(conexion == null || conexion.isClosed()){
+                crearConexion();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error al comprobar el estado de la conexión");
+            System.out.println(e.getMessage());
+        }
+        return conexion;
+    }
+
+    private static void crearConexion(){
+        String database = "breakpoint";
+        String user = "root";
+        String pass = "";
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+ database, user, pass);
+        } catch (SQLException e) {
+            System.out.println("Error en la conexión con la base de datos");
+            System.out.println(e.getMessage());
+        }
+    }
 }
