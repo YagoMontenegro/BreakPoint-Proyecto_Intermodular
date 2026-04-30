204bf5c38c7de2d6b1470fd80a4f477ea3c5e489
Yago Montenegro <y.montenegrodf@gmail.com>
2026-04-30 17:02:21 +0200

    Se añade MenuPrincipalView.java

---
+++ app/breakpiont/src/main/java/view/MenuPrincipalView.java
@@ -0,0 +1,18 @@
+package view;
+
+public class MenuPrincipalView {
+
+    public void mostrarMenuPrincipal() {
+        System.out.println("--- BREAKPOINT – CLUB DE BILLAR ---");
+        System.out.println("Menú de Gestión");
+        System.out.println("1. Gestionar usuarios");
+        System.out.println("2. Gestionar socios");
+        System.out.println("3. Gestionar cuotas");
+        System.out.println("4. Gestionar mesas");
+        System.out.println("5. Gestionar reservas");
+        System.out.println("6. Gestionar torneos");
+        System.out.println("7. Gestionar inscripciones");
+        System.out.println("0. Salir");
+        System.out.print("Por favor, selecciona una opción: ");
+    }
+}
