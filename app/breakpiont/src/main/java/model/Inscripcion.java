34a7b430ca89b79be61667b2266ea3e9f297e5b1
Yago Montenegro <y.montenegrodf@gmail.com>
2026-04-30 16:33:38 +0200

    Se añade la clase modelo Inscripcion.java

---
+++ app/breakpiont/src/main/java/model/Inscripcion.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.time.LocalDateTime;
+
+public class Inscripcion {
+    private Socio socio;
+    private Torneo torneo;
+    private LocalDateTime fechaInscripcion;
+    private Integer resultado;
+
+    public Inscripcion() {
+
+    }
+
+    public Inscripcion(Socio socio, Torneo torneo, LocalDateTime fechaInscripcion, Integer resultado) {
+        this.socio = socio;
+        this.torneo = torneo;
+        this.fechaInscripcion = fechaInscripcion;
+        this.resultado = resultado;
+    }
+
+    @Override
+    public String toString() {
+        return  "\n--- INSCRIPCIÓN ---" +
+                "\nID Socio: " + socio.getIdSocio() +
+                "\nNombre socio: " + socio.getUsuario().getNombre() + " " + socio.getUsuario().getApellidos() +
+                "\nID Torneo: " + torneo.getIdTorneo() +
+                "\nTorneo: " + torneo.getNombre() +
+                "\nFecha de inscripción: " + fechaInscripcion +
+                "\nResultado: " + (resultado != null ? resultado : "Pendiente de resolución");
+    }
+
+    public Socio getSocio() {
+        return socio;
+    }
+
+    public void setSocio(Socio socio) {
+        this.socio = socio;
+    }
+
+    public Torneo getTorneo() {
+        return torneo;
+    }
+
+    public void setTorneo(Torneo torneo) {
+        this.torneo = torneo;
+    }
+
+    public LocalDateTime getFechaInscripcion() {
+        return fechaInscripcion;
+    }
+
+    public void setFechaInscripcion(LocalDateTime fechaInscripcion) {
+        this.fechaInscripcion = fechaInscripcion;
+    }
+
+    public Integer getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Integer resultado) {
+        this.resultado = resultado;
+    }
+}
