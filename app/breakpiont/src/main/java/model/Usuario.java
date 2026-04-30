77ce306ad6bcec6ce4bc2454ce8f0923d7eadf03
Yago Montenegro <y.montenegrodf@gmail.com>
2026-04-30 16:28:23 +0200

    Se añade la clase modelo Usuario.java

--- app/breakpiont/src/main/java/model/Usuario.java
+++ app/breakpiont/src/main/java/model/Usuario.java
@@ -1,4 +1,84 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class Usuario {
+    private int idUsuario;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String telefono;
+    private LocalDateTime fechaRegistro;
+
+    public Usuario() {
+
+    }
+
+    public Usuario(int idUsuario, String nombre, String apellidos, String email, String telefono, LocalDateTime fechaRegistro) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    @Override
+    public String toString() {
+        return  "\n--- USUARIO ---" +
+                "\nID Usuario: " + idUsuario +
+                "\nNombre: " + nombre +
+                "\nApellidos: " + apellidos +
+                "\nEmail: " + email +
+                "\nTeléfono: " + telefono +
+                "\nFecha de registro: " + fechaRegistro;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public LocalDateTime getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDateTime fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
 }
