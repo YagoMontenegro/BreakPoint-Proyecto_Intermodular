51632276610cd92d467948bc674cd83f6b1d30aa
Yago Montenegro <y.montenegrodf@gmail.com>
2026-05-01 15:13:40 +0200

    Se modifica menú en CuotaSocioView

--- app/breakpiont/src/main/java/view/CuotaSocioView.java
+++ app/breakpiont/src/main/java/view/CuotaSocioView.java
@@ -7,7 +7,7 @@ public class CuotaSocioView {
         System.out.println("1. Registrar pago de cuota");
         System.out.println("2. Listar cuotas");
         System.out.println("3. Buscar cuota");
-        System.out.println("4. Modificar cuota");
+        System.out.println("4. Pagar cuota pendiente");
         System.out.println("5. Eliminar cuota");
         System.out.println("0. Volver");
         System.out.print("Selecciona una opción: ");
